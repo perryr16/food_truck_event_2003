b4a8cf3110daa28135b4149493ee2ad500a25305
Ross Perry <perryr16@gmail.com>
2020-04-15 10:29:34 -0600

    food trucks add

--- test/event_test.rb
+++ test/event_test.rb
@@ -20,51 +20,32 @@ class EventTest < Minitest::Test
   def test_it_adds_food_trucks
     event = Event.new("South Pearl Street Farmers Market")
     assert_equal [], event.food_trucks
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected  = [food_truck1, food_truck2, food_truck3]
+    assert_equal expected, event.food_trucks
+
   end
 
 end
 
 
-# #=> []
+
+
 #
-#food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-# #=> #<FoodTruck:0x00007fe1348a1160...>
-#
-#item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-#item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-#item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-#item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-#food_truck1.stock(item1, 35)
-#
-#food_truck1.stock(item2, 7)
-#
-#food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-# #=> #<FoodTruck:0x00007fe1349bed40...>
-#
-#food_truck2.stock(item4, 50)
-#
-#food_truck2.stock(item3, 25)
-#
-#food_truck3 = FoodTruck.new("Palisade Peach Shack")
-# #=> #<FoodTruck:0x00007fe134910650...>
-#
-#food_truck3.stock(item1, 65)
-#
-#event.add_food_truck(food_truck1)
-#
-#event.add_food_truck(food_truck2)
-#
-#event.add_food_truck(food_truck3)
-#
-#event.food_trucks
 # #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe1349bed40...>, #<FoodTruck:0x00007fe134910650...>]
 #
 #event.food_truck_names
