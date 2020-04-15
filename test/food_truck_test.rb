74cb08d224bcc35c64b7d6dbe6be07f8c9b2e45f
Ross Perry <perryr16@gmail.com>
2020-04-15 10:09:43 -0600

    attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,32 +8,26 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
 
   def test_it_exists
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_instance_of FoodTruck, food_truck
+  end
+
+  def test_it_returns_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal "Rocky Mountain Pies", food_truck.name
   end
 
 end
 
 
-#item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-#item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-#item2.name
-# #=> "Apple Pie (Slice)"
-#
-#item2.price
-# #=> 2.50
-#
-#food_truck = FoodTruck.new("Rocky Mountain Pies")
+
 # #=> #<FoodTruck:0x00007f85683152f0...>
 #
-#food_truck.name
 # #=> "Rocky Mountain Pies"
-#
-#food_truck.inventory
+#    food_truck.inventory
 # #=> {}
 #
 #food_truck.check_stock(item1)
