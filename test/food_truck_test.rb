db694426c8284e4d7174ba50bd9aa64722d51cc5
Ross Perry <perryr16@gmail.com>
2020-04-15 10:15:09 -0600

    stock n inventory

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -20,25 +20,31 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
   end
 
-  def test_it_returns_and_adds_inventory
+  def test_it_returns_inventory
     food_truck = FoodTruck.new("Rocky Mountain Pies")
-    
     assert_equal ({}), food_truck.inventory
+  end
+
+  def test_it_checks_stock_n_inventory
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal 0, food_truck.check_stock(item1)
+    food_truck.stock(item1, 30)
+    assert_equal [item1], food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+
+
   end
 
 end
 
 
 
-#food_truck.check_stock(item1)
 # #=> 0
 #
-#food_truck.stock(item1, 30)
 #
-#food_truck.inventory
 # #=> {#<Item:0x007f9c56740d48...> => 30}
 #
-#food_truck.check_stock(item1)
 # #=> 30
 #
 #food_truck.stock(item1, 25)
