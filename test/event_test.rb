6c9f2cb11e7b58759e545b6861bca2123a5fdd0e
Ross Perry <perryr16@gmail.com>
2020-04-15 10:34:16 -0600

    trucks that sell

--- test/event_test.rb
+++ test/event_test.rb
@@ -57,9 +57,33 @@ class EventTest < Minitest::Test
     event.add_food_truck(food_truck1)
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
-    
+
     expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     assert_equal expected, event.food_truck_names
+  end
+
+  def test_it_returns_trucks_that_sell_item
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = [food_truck1, food_truck3]
+    event.food_trucks_that_sell(item1)
+    expected = [food_truck2]
+    event.food_trucks_that_sell(item4)
+
 
   end
 
@@ -67,16 +91,9 @@ end
 
 
 
-
-
-#event.food_truck_names
-# #=> ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+# #=>
 #
-#event.food_trucks_that_sell(item1)
-# #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe134910650...>]
-#
-#event.food_trucks_that_sell(item4)
-# #=> [#<FoodTruck:0x00007fe1349bed40...>]
+# #=>
 #
 #food_truck1.potential_revenue
 # #=> 148.75
