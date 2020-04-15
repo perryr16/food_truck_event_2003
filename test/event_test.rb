3546fe28bb8f01c9087bde0dff9ef5d7cb7dc1d7
Ross Perry <perryr16@gmail.com>
2020-04-15 10:51:00 -0600

    total inventory

--- test/event_test.rb
+++ test/event_test.rb
@@ -103,10 +103,49 @@ class EventTest < Minitest::Test
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
     assert_equal 148.75, food_truck1.potential_revenue
-
     assert_equal 345.00, food_truck2.potential_revenue
-
     assert_equal 243.75, food_truck3.potential_revenue
+  end
+
+  def test_case_name
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expected = {
+                item1 => {
+                  quantity: 100,
+                  food_trucks: [food_truck1, food_truck3]
+                },
+                item2 => {
+                  quantity: 7,
+                  food_trucks: [food_truck1]
+                },
+                item4 => {
+                  quantity: 50,
+                  food_trucks: [food_truck2]
+                },
+                item3 => {
+                  quantity: 35,
+                  food_trucks: [food_truck2, food_truck3]
+                },
+              }
+
+    assert_equal expected, event.total_inventory
 
   end
 
@@ -114,7 +153,28 @@ end
 
 
 
-# #=>
 #
-# #=>
+# #=> {
+#   #   #<Item:0x007f9c56740d48...> => {
+#   #     quantity: 100,
+#   #     food_trucks: [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe134910650...>]
+#   #   },
+#   #   #<Item:0x007f9c565c0ce8...> => {
+#   #     quantity: 7,
+#   #     food_trucks: [#<FoodTruck:0x00007fe1348a1160...>]
+#   #   },
+#   #   #<Item:0x007f9c56343038...> => {
+#   #     quantity: 50,
+#   #     food_trucks: [#<FoodTruck:0x00007fe1349bed40...>]
+#   #   },
+#   #   #<Item:0x007f9c562a5f18...> => {
+#   #     quantity: 35,
+#   #     food_trucks: [#<FoodTruck:0x00007fe1349bed40...>, #<FoodTruck:0x00007fe134910650...>]
+#   #   },
+#   # }
 #
+# pry(main).overstocked_items
+# #=> [#<Item:0x007f9c56740d48...>]
+#
+#event.sorted_item_list
+# #=> ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
