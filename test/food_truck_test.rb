76541be498e90c7f8b99ab52db8dfe4e78a71ae2
Ross Perry <perryr16@gmail.com>
2020-04-15 10:20:18 -0600

    iteration 1

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,8 +8,7 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
 
   def test_it_exists
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
 
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_instance_of FoodTruck, food_truck
@@ -27,33 +26,27 @@ class FoodTruckTest < Minitest::Test
 
   def test_it_checks_stock_n_inventory
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_equal 0, food_truck.check_stock(item1)
     food_truck.stock(item1, 30)
     expected = {item1 => 30}
     assert_equal expected, food_truck.inventory
     assert_equal 30, food_truck.check_stock(item1)
-
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+    food_truck.stock(item2, 12)
+    expected = {item1 => 55, item2 => 12}
+    assert_equal expected, food_truck.inventory
 
   end
 
 end
 
 
-
-# #=> 0
 #
 #
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-# #=> 30
-#
-#food_truck.stock(item1, 25)
-#
-#food_truck.check_stock(item1)
 # #=> 55
 #
-#food_truck.stock(item2, 12)
 #
-#food_truck.inventory
 # #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
